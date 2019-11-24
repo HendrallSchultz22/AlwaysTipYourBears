244622a34128cfc615595f100ce33c0f02b5b35c
Jeremy <colearkenach@gmail.com>
2019-11-24 09:28:23 -0500

    Add files via upload

---
+++ Project/ScriptDevelopment/Assets/Scripts/AutoDelete.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDelete : MonoBehaviour
+{
+    [Header("Controls")]
+    public bool isActivated = false;
+    public float timeFrame; // How much time until it's despawned.
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Despawn player if they enter through the trigger.
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject.GetComponentInParent<SolidSurface>().gameObject, timeFrame);
+        }
+    }
+
+}
