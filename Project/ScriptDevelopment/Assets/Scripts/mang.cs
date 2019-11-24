29a1e94141fec4e35013497f1b5bbdd9bd6d8d04
zacharySpitzer <45111466+zacharySpitzer@users.noreply.github.com>
2019-11-24 15:24:53 -0500

    yeeeeeeee

---
+++ Project/ScriptDevelopment/Assets/Scripts/mang.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mang : MonoBehaviour
+{
+    
+    public float count = 0;
+
+}
---
+++ Project/ScriptDevelopment/Assets/Scripts/spawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+
+    public GameObject hall;
+    public List<Transform> spawnpoints;
+    private List<GameObject> halls;
+    public GameObject manager;
+
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameController");
+       if (manager.GetComponent<mang>().count < 100)
+        {
+            generate();
+        }
+    }
+    void generate()
+    {
+        foreach (Transform t in spawnpoints)
+        {
+            Instantiate(hall, t.position, t.rotation);
+            manager.GetComponent<mang>().count++;
+        }
+    }
+}
