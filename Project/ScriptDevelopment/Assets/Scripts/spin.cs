9760e04a936e3fa30de3c521a7f9e1ffe6c13bb7
zacharySpitzer <45111466+zacharySpitzer@users.noreply.github.com>
2019-11-24 11:37:48 -0500

    Create spin.cs

---
+++ Project/ScriptDevelopment/Assets/Scripts/spin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    public GameObject spinPoint;
+    public Vector3 SpinRatio = Vector3.zero;
+    public bool point = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spinning();
+    }
+    public void spinning()
+    {
+        if(point)
+        {
+            if (spinPoint.transform.eulerAngles.y >= 180)
+            {
+                point = false;
+            }
+            spinPoint.transform.Rotate(SpinRatio * Time.deltaTime);
+        }
+     
+    }
+}
